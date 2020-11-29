d11bf1b10e4efd9b6fad8e0ffcef5c611bf59c84
Bobbie Ysabel Benedito <b.y.benedito@gmail.com>
2020-11-28 23:56:19 -0500

    configure date formatting plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const pluginDate = require("eleventy-plugin-date");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css')
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPlugin(pluginDate);
     return {
       passthroughFileCopy: true
     }
